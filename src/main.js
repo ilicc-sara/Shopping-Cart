4ebd85e331ab2047698d10b209da4c1423daa629
Sara <sarailic431@gmail.com>
2025-01-10 19:01:21 +0100

    iterate over cars array to make function and adding pictures of cars

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const cars = [
     doors: 4,
     price: 22000,
     available: "yes",
-    image: "./img/first car toyota.jpg",
+    image: "images/first car toyota.jpg",
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ const cars = [
     doors: 4,
     price: 25000,
     available: "yes",
-    image: "./img/honda civic second.webp",
+    image: "images/honda civic second.webp",
   },
   {
     id: 3,
@@ -30,7 +30,7 @@ const cars = [
     doors: 2,
     price: 35000,
     available: "no",
-    image: "./img/mustant 3.png",
+    image: "images/mustant 3.png",
   },
   {
     id: 4,
@@ -40,7 +40,7 @@ const cars = [
     doors: 4,
     price: 45000,
     available: "yes",
-    image: "./img/BMW 3 Series 4.png",
+    image: "images/BMW 3 Series 4.png",
   },
   {
     id: 5,
@@ -50,7 +50,7 @@ const cars = [
     doors: 2,
     price: 32000,
     available: "no",
-    image: "./img/Chavrolet 5.jpg",
+    image: "images/Chavrolet 5.jpg",
   },
   {
     id: 6,
@@ -60,7 +60,7 @@ const cars = [
     doors: 4,
     price: 42000,
     available: "yes",
-    image: "./img/audi a4 6.jpg",
+    image: "images/audi a4 6.jpg",
   },
   {
     id: 7,
@@ -70,7 +70,7 @@ const cars = [
     doors: 4,
     price: 50000,
     available: "yes",
-    image: "./img/mercedes e klasa 7.jpg",
+    image: "images/mercedes e klasa 7.jpg",
   },
   {
     id: 8,
@@ -80,7 +80,7 @@ const cars = [
     doors: 4,
     price: 38000,
     available: "no",
-    image: "./img/lexus 8.webp",
+    image: "images/lexus 8.webp",
   },
   {
     id: 9,
@@ -90,7 +90,7 @@ const cars = [
     doors: 4,
     price: 28000,
     available: "yes",
-    image: "./img/golf 9.jpg",
+    image: "images/golf 9.jpg",
   },
   {
     id: 10,
@@ -100,14 +100,14 @@ const cars = [
     doors: 4,
     price: 32000,
     available: "yes",
-    image: "./img/subaru 10.png",
+    image: "images/subaru 10.png",
   },
 ];
 
 const carCont = document.querySelector(".car-cont");
 const carList = document.querySelector("car-lists");
 
-console.log(cars);
+// console.log(cars);
 
 const carCreator = function () {
   const car = {
@@ -120,26 +120,40 @@ const carCreator = function () {
     available: true,
     img: "",
   };
+  const getCar = () => car;
   const getId = () => car.id;
   const getName = () => car.name;
+  const setName = (value) => (car.name = value);
   const getBrand = () => car.brand;
+  const setBrand = (value) => (car.brand = value);
+
   const getYear = () => car.year;
+  const setYear = (value) => (car.year = value);
+
   const getDoors = () => car.doors;
+  const setDoors = (value) => (car.doors = value);
   const getPrice = () => car.price;
+  const setPrice = (value) => (car.price = value);
   const getAvlbl = () => car.available;
-  const changeStatus = (value) => (car.available = value);
+  const setAvlbl = (value) => (car.available = value);
   const getImg = () => car.img;
   const setImg = (value) => (car.img = value);
 
   return {
+    getCar,
     getId,
     getName,
+    setName,
     getBrand,
+    setBrand,
     getYear,
+    setYear,
     getDoors,
+    setDoors,
     getPrice,
+    setPrice,
     getAvlbl,
-    changeStatus,
+    setAvlbl,
     getImg,
     setImg,
   };
@@ -151,9 +165,20 @@ const carCreator = function () {
 //   const cars = [];
 // };
 
-const carItem = function () {
-  for (let i; i > cars.length; i++) {
-    console.log([i]);
-  }
-};
-carItem();
+// const carItem = function () {
+for (let i = 0; i < cars.length; i++) {
+  // console.log(cars[i].name);
+
+  const car = carCreator();
+
+  car.setName(cars[i].name);
+  car.setBrand(cars[i].brand);
+  car.setYear(cars[i].manufacturedYear);
+  car.setDoors(cars[i].doors);
+  car.setPrice(cars[i].price);
+  car.setAvlbl(cars[i].available);
+  car.setImg(cars[i].image);
+  console.log(car.getCar());
+}
+// };
+// carItem();
